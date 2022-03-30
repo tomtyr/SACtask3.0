ce9239c93c501724e1be282c57b33dc666caf757
tomtyr <102113945+tomtyr@users.noreply.github.com>
2022-03-30 14:01:58 +1100

    Add files via upload

---
+++ SACtask3/SACtask3/Sales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SACtask3
+{
+    internal class Sales
+    {
+        public string ItemName { get; set; }
+        public string Subject { get; set; }
+        public string Seller { get; set; }
+        public string Purchaser { get; set; }
+        public float Purchase { get; set; }
+        public string Sale { get; set; }
+        public string Rating { get; set; }
+
+
+
+    }
+}
