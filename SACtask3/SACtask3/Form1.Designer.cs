3dd75c3aec1f0a7d14bb636e8796263f62066579
tomtyr <102113945+tomtyr@users.noreply.github.com>
2022-03-30 13:58:26 +1100

    Add files via upload

---
+++ SACtask3/SACtask3/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SACtask3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbo_filter = new System.Windows.Forms.ComboBox();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter by:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cbo_filter
+            // 
+            this.cbo_filter.FormattingEnabled = true;
+            this.cbo_filter.Items.AddRange(new object[] {
+            "Textbook",
+            "Subject",
+            "Rating"});
+            this.cbo_filter.Location = new System.Drawing.Point(104, 33);
+            this.cbo_filter.Name = "cbo_filter";
+            this.cbo_filter.Size = new System.Drawing.Size(121, 28);
+            this.cbo_filter.TabIndex = 1;
+            this.cbo_filter.SelectedIndexChanged += new System.EventHandler(this.cbo_filter_SelectedIndexChanged);
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(289, 35);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(158, 26);
+            this.txt_search.TabIndex = 2;
+            this.txt_search.TextChanged += new System.EventHandler(this.txt_search_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1213, 708);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1259, 810);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.cbo_filter);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbo_filter;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SACtask3/SACtask3/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SACtask3
+{
+    public partial class Form1 : Form
+    {
+
+        string filter;
+        List<Sales> sales = new List<Sales>();
+        BindingSource bs = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCSV();
+            bs.DataSource = sales;
+            dataGridView1.DataSource = bs;
+        }
+
+        private void LoadCSV()
+        {
+            string filePath = @"C:\Users\tomty\OneDrive\Documents\school\compu\Task3_Shop_Data.csv";
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filePath).ToList();
+            foreach (string line in lines)
+            {
+                List<string> fields = line.Split(',').ToList();
+                Sales s = new Sales();
+                s.ItemName = fields[0];
+                s.Subject = fields[1];
+                s.Seller = fields[2];
+                s.Purchaser = fields[3];
+                s.Purchase = float.Parse(fields[4]);
+                s.Sale = fields[5];
+                s.Rating = fields[6];
+                sales.Add(s);
+            }
+
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+
+        {
+
+        }
+
+        private void cbo_filter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filter = cbo_filter.Text;
+            if (filter == "Rating") SortByRating(sales);
+            dataGridView1.DataSource = bs;
+            bs.ResetBindings(false);
+        }
+
+        private void SortByRating(List<Sales> _sales)
+        {
+            int min;
+            string temp;
+            for (int i = 0; i < _sales.Count - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < _sales.Count; j++)
+                {
+
+                    if (int.TryParse(_sales[j].Rating, out int ratingJ))
+                    {
+
+                        if (int.TryParse(_sales[min].Rating, out int ratingMin))
+                        { if (ratingJ < ratingMin) min = j; }
+                        else
+                        {
+                            min = j;
+                        }
+                    }
+                }
+                temp = _sales[min].Rating;
+                _sales[min].Rating = _sales[i].Rating;
+                _sales[i].Rating = temp;
+
+
+            }
+        }
+
+        private List<Sales> Search(string target, string filter)
+        {
+            List<Sales> results = new List<Sales>();
+            foreach (Sales s in sales)
+            {
+                if (filter == "Rating")
+                {
+                    if (s.Rating.ToLower() == target.ToLower()) results.Add(s);
+                }
+                if (filter == "Subject")
+                {
+                    if (s.Subject.ToLower().Contains(target.ToLower())) { results.Add(s); Console.WriteLine(target); }
+                }
+                if (filter == "Textbook")
+                {
+                    if (s.ItemName.ToLower().Contains(target.ToLower())) { results.Add(s); Console.WriteLine(target); }
+                }
+
+            }
+            return results;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txt_search_TextChanged(object sender, EventArgs e)
+        {
+            List<Sales> r = Search(txt_search.Text, filter);
+            bs.DataSource = r;
+            dataGridView1.DataSource = r;
+            bs.ResetBindings(false);
+        }
+    }
+}
